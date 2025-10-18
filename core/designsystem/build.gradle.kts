604032d9eda30a17c946bb96ef57dbdb5b829c01
kamja0510 <kamja990510@gmail.com>
2025-10-19 03:51:56 +0900

    [refactor] #6 designsystem 모듈 생성

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
@@ -12,7 +12,6 @@ android {
         minSdk = 28
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
