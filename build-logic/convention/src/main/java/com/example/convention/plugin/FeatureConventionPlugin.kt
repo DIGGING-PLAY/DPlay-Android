1906fa3ba5525912d16580ba2b9e4887413ec70a
kamja0510 <kamja990510@gmail.com>
2025-10-20 02:29:26 +0900

    [init] #9 feature 레이어의 모듈들의 공통 설정 구현

---
+++ build-logic/convention/src/main/java/com/example/convention/plugin/FeatureConventionPlugin.kt
@@ -0,0 +1,24 @@
+package com.example.convention.plugin
+
+import com.example.convention.util.getBundle
+import com.example.convention.util.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class FeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                apply("dplay.android.compose")
+            }
+
+            dependencies {
+                add("implementation",libs.getBundle("compose"))
+                add("implementation",libs.getBundle("navigation"))
+                add("implementation",project(":core:designsystem"))
+                add("implementation",project(":core:common"))
+            }
+        }
+    }
+}
