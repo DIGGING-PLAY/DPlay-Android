5b9ced6caf553c376a9fc065be8561b9df21e838
kamja0510 <kamja990510@gmail.com>
2025-10-20 02:25:18 +0900

    [init] #9 build-logic을 빌드 루트로 사용하기 위한 설정

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.compose.compiler.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+}
