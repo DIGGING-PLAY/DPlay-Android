b6c82377b915515629bfa7b3f72347531b1a21a5
kamja0510 <kamja990510@gmail.com>
2025-10-20 02:38:02 +0900

    [init] #9 MainActivity.kt feature:main 모듈로 옮기고 DPlayApplication 생성해 시작점 정의

--- feature/main/src/main/java/com/example/main/MainActivity.kt
+++ feature/main/src/main/java/com/example/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dplay
+package com.example.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,18 +11,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.designsystem.theme.DPlayTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            DPlayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
+            }
         }
     }
 }
