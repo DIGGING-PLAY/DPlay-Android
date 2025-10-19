7de26cfa45f89e9e3accb48d1e8fbdb8730c1d3a
kamja0510 <kamja990510@gmail.com>
2025-10-20 02:30:26 +0900

    [init] #9 만든 컨벤션 플러그인 등록

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -13,3 +13,42 @@ dependencies {
     compileOnly(libs.compose.compiler.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
 }
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "dplay.android.application"
+            implementationClass = "com.example.convention.plugin.AndroidApplicationConventionPlugin"
+        }
+
+        register("androidLibrary") {
+            id = "dplay.android.library"
+            implementationClass = "com.example.convention.plugin.AndroidLibraryConventionPlugin"
+        }
+
+        register("androidCompose") {
+            id = "dplay.android.compose"
+            implementationClass = "com.example.convention.plugin.AndroidComposeConventionPlugin"
+        }
+
+        register("feature") {
+            id = "dplay.feature"
+            implementationClass = "com.example.convention.plugin.FeatureConventionPlugin"
+        }
+
+        register("data") {
+            id = "dplay.data"
+            implementationClass = "com.example.convention.plugin.DataConventionPlugin"
+        }
+
+        register("domain"){
+            id = "dplay.domain"
+            implementationClass = "com.example.convention.plugin.JavaLibraryPlugin"
+        }
+
+        register("hilt") {
+            id = "dplay.hilt"
+            implementationClass = "com.example.convention.plugin.HiltConventionPlugin"
+        }
+    }
+}
