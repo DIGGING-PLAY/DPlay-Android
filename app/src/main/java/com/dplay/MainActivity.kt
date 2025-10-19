135de71324099e516810001118735786b4f2fd08
t1nm1ksun <minsuk07672@naver.com>
2025-10-19 16:33:53 +0900

    [init] #8 컬러테마 추가

--- app/src/main/java/com/dplay/MainActivity.kt
+++ app/src/main/java/com/dplay/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.designsystem.theme.DPlayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +32,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        color = DPlayTheme.colors.dplayPink
     )
 }
 
