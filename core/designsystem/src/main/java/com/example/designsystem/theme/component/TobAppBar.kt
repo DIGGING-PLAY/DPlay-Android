9b512a81ac537c7f12d74c9db57f021c9c1c0e35
minseokshin07672 <122257945+t1nm1ksun@users.noreply.github.com>
2025-11-02 18:11:18 +0900

    [feat] #17 아이콘 패딩 추가

--- core/designsystem/src/main/java/com/example/designsystem/theme/component/TobAppBar.kt
+++ core/designsystem/src/main/java/com/example/designsystem/theme/component/TobAppBar.kt
@@ -1,9 +1,11 @@
 package com.example.designsystem.theme.component
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Text
@@ -15,6 +17,7 @@ import androidx.compose.ui.unit.dp
 import com.dplay.designsystem.R
 import com.example.designsystem.theme.icon.DplayBaseIcon
 import com.example.designsystem.theme.icon.DplayClickableIcon
+import com.example.designsystem.theme.theme.DPlayTheme
 import com.example.designsystem.theme.util.noRippleClickable
 
 @Composable
@@ -26,23 +29,34 @@ fun DplayTopAppBar(
     onLeftClick: (() -> Unit)? = null,
     onRightClick: (() -> Unit)? = null,
 ) {
+
+    val iconPaddingModifier = Modifier.padding(12.dp)
+
     CenterAlignedTopAppBar(
         modifier = modifier,
         title = { title?.let { Text(text = it) } },
         navigationIcon = {
             leftIconRes?.let {
                 if (onLeftClick != null)
-                    DplayClickableIcon(iconRes = it, onClick = onLeftClick)
+                    DplayClickableIcon(
+                        modifier = iconPaddingModifier,
+                        iconRes = it,
+                        onClick = onLeftClick
+                    )
                 else
-                    DplayBaseIcon(iconRes = it)
+                    DplayBaseIcon(modifier = iconPaddingModifier, iconRes = it)
             }
         },
         actions = {
             rightIconRes?.let {
                 if (onRightClick != null)
-                    DplayClickableIcon(iconRes = it, onClick = onRightClick)
+                    DplayClickableIcon(
+                        modifier = iconPaddingModifier,
+                        iconRes = it,
+                        onClick = onRightClick
+                    )
                 else
-                    DplayBaseIcon(iconRes = it)
+                    DplayBaseIcon(modifier = iconPaddingModifier, iconRes = it)
             }
         }
     )
@@ -52,13 +66,24 @@ fun DplayTopAppBar(
 @Composable
 fun DplayLogoTopAppBar(
     modifier: Modifier = Modifier,
-    onListClick: (() -> Unit)? = null
+    onListClick: (() -> Unit)
 ) {
-    DplayTopAppBar(
+    val iconPaddingModifier = Modifier.padding(12.dp)
+
+    CenterAlignedTopAppBar(
         modifier = modifier,
-        leftIconRes = R.drawable.ic_wordmark_pink,
-        rightIconRes = R.drawable.ic_list_24,
-        onRightClick = onListClick
+        navigationIcon = {
+            DplayBaseIcon(modifier = iconPaddingModifier.padding(start = 4.dp), iconRes = R.drawable.ic_wordmark_pink)
+
+        },
+        actions = {
+            DplayClickableIcon(
+                modifier = iconPaddingModifier,
+                iconRes = R.drawable.ic_list_24,
+                onClick = onListClick
+            )
+        },
+        title = {}
     )
 }
 
@@ -176,25 +201,27 @@ fun DplayTitleButtonTopAppBar(
 @Preview(showBackground = true)
 @Composable
 private fun PreviewDplayTopAppBars() {
-    androidx.compose.foundation.layout.Column {
-        DplayLogoTopAppBar(onListClick = {})
-        Spacer(modifier = Modifier.height(8.dp))
+    DPlayTheme {
+        Column {
+            DplayLogoTopAppBar(onListClick = {})
+            Spacer(modifier = Modifier.height(36.dp))
 
-        DplayLeftIconTopAppBar(onLeftClick = {})
-        Spacer(modifier = Modifier.height(8.dp))
+            DplayLeftIconTopAppBar(onLeftClick = {})
+            Spacer(modifier = Modifier.height(36.dp))
 
-        DplayDualIconTopAppBar(onLeftClick = {}, onRightClick = {})
-        Spacer(modifier = Modifier.height(8.dp))
+            DplayDualIconTopAppBar(onLeftClick = {}, onRightClick = {})
+            Spacer(modifier = Modifier.height(36.dp))
 
-        DplayRightIconTitleTopAppBar(title = "설정", onRightClick = {})
-        Spacer(modifier = Modifier.height(8.dp))
+            DplayRightIconTitleTopAppBar(title = "Title", onRightClick = {})
+            Spacer(modifier = Modifier.height(36.dp))
 
-        DplayLeftIconTitleTopAppBar(title = "뒤로가기", onLeftClick = {})
-        Spacer(modifier = Modifier.height(8.dp))
+            DplayLeftIconTitleTopAppBar(title = "Title", onLeftClick = {})
+            Spacer(modifier = Modifier.height(36.dp))
 
-        DplayDualIconTitleTopAppBar(title = "탑바 예시", onLeftClick = {}, onRightClick = {})
-        Spacer(modifier = Modifier.height(8.dp))
+            DplayDualIconTitleTopAppBar(title = "Title", onLeftClick = {}, onRightClick = {})
+            Spacer(modifier = Modifier.height(36.dp))
 
-        DplayTitleButtonTopAppBar(title = "정렬", onLeftClick = {}, onButtonClick = {})
+            DplayTitleButtonTopAppBar(title = "Title", onLeftClick = {}, onButtonClick = {})
+        }
     }
 }
