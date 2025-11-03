f93f6dfa4b18f2dded05ec3893629f58258c3b4c
kamja0510 <kamja990510@gmail.com>
2025-11-03 22:43:26 +0900

    [init] #21 Dummy 패키지 선언

---
+++ feature/dummy/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.dplay.feature)
+    alias(libs.plugins.dplay.hilt)
+    alias(libs.plugins.dplay.test)
+}
+
+android {
+    namespace = "com.dplay.dummy"
+}
+
+dependencies {
+    implementation(projects.core.ui)
+}
