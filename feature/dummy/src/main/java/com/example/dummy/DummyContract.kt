7fc63b6f3b2e16d2336a2a62181d3aafe20755f4
kamja0510 <kamja990510@gmail.com>
2025-11-03 22:43:43 +0900

    [init] #21 Dummy Contract 구현

---
+++ feature/dummy/src/main/java/com/example/dummy/DummyContract.kt
@@ -0,0 +1,20 @@
+package com.example.dummy
+
+import com.example.ui.base.BaseContract
+
+class DummyContract {
+    data class DummyState(
+        val loading: Boolean = false,
+        val count: Int = 0,
+    ) : BaseContract.State
+
+    sealed interface DummyIntent : BaseContract.Intent {
+        data object Initialize : DummyIntent
+
+        data class OnClickNumberButton(val number: Int) : DummyIntent
+    }
+
+    sealed interface DummySideEffect : BaseContract.SideEffect {
+        data class ShowSnackBar(val message: String) : DummySideEffect
+    }
+}
