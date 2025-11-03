da438740a0d892a2e3df6685588c7b6b1cdd9d93
kamja0510 <kamja990510@gmail.com>
2025-11-03 22:43:03 +0900

    [init] #21 BaseViewModel 구현

---
+++ core/ui/src/main/java/com/example/ui/base/BaseViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.ui.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<STATE : BaseContract.State, INTENT : BaseContract.Intent, SIDE_EFFECT : BaseContract.SideEffect>(
+    initialState: STATE,
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(initialState)
+    val uiState = _uiState.asStateFlow()
+
+    private val _sideEffect: Channel<SIDE_EFFECT> = Channel(Channel.BUFFERED)
+    val sideEffect = _sideEffect.receiveAsFlow()
+
+    protected val currentState: STATE
+        get() = _uiState.value
+
+    abstract fun handleIntent(intent: INTENT)
+
+    protected fun updateState(reducer: STATE.() -> STATE) {
+        _uiState.update { currentState.reducer() }
+    }
+
+    protected fun setSideEffect(effect: SIDE_EFFECT) {
+        viewModelScope.launch {
+            _sideEffect.send(effect)
+        }
+    }
+}
