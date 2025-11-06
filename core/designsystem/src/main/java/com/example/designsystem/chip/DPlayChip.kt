56951c5d0d114fc1d64479beb30bd2d68f0137f5
kamja0510 <kamja990510@gmail.com>
2025-11-07 03:06:31 +0900

    [feat] #31 chip 구현

---
+++ core/designsystem/src/main/java/com/example/designsystem/chip/DPlayChip.kt
@@ -0,0 +1,81 @@
+package com.example.designsystem.chip
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.designsystem.chip.type.DPlayChipType
+import com.example.designsystem.theme.DPlayTheme
+
+@Composable
+fun DPlayChip(
+    type: DPlayChipType,
+    modifier: Modifier = Modifier,
+){
+    Box(
+        modifier = modifier
+            .clip(
+                shape = RoundedCornerShape(16.dp)
+            )
+            .border(
+                width = 1.dp,
+                color = DPlayTheme.colors.dplayPink,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .background(DPlayTheme.colors.dplayWhite)
+            .padding(horizontal = 12.dp,vertical = 6.dp),
+
+    ){
+        Row(){
+            Icon(
+                imageVector = ImageVector.vectorResource(type.iconRes),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+
+            Spacer(modifier = Modifier.width(4.dp))
+
+            Text(
+                text = stringResource(type.stringRes),
+                style = DPlayTheme.typography.bodySemi14,
+                color = DPlayTheme.colors.dplayPink
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DPlayChipPreview(){
+    DPlayTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(DPlayTheme.colors.dplayWhite)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ){
+            DPlayChip(type = DPlayChipType.NEW)
+            DPlayChip(type = DPlayChipType.EDITOR)
+            DPlayChip(type = DPlayChipType.BEST)
+        }
+    }
+}
