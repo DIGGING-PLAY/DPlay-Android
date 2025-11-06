a151bac92b9b1e3d745c4c2d762f905988253575
kamja0510 <kamja990510@gmail.com>
2025-11-07 03:05:56 +0900

    [feat] #31 chip 타입 정의

---
+++ core/designsystem/src/main/java/com/example/designsystem/chip/type/DPlayChipType.kt
@@ -0,0 +1,23 @@
+package com.example.designsystem.chip.type
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.dplay.designsystem.R
+
+enum class DPlayChipType(
+    @DrawableRes val iconRes: Int,
+    @StringRes val stringRes: Int,
+) {
+    EDITOR(
+        iconRes = R.drawable.ic_editor_20,
+        stringRes = R.string.chip_editor,
+    ),
+    NEW(
+        iconRes = R.drawable.ic_new_20,
+        stringRes = R.string.chip_new,
+    ),
+    BEST(
+        iconRes = R.drawable.ic_best_20,
+        stringRes = R.string.chip_best,
+    ),
+}
