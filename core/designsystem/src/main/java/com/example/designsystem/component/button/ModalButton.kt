c7f7900a4ff3135ee36e397d0c34c59b0f78fbff
kamja0510 <kamja990510@gmail.com>
2025-11-07 01:18:10 +0900

    [feat] #27 모달용 버튼 구현

---
+++ core/designsystem/src/main/java/com/example/designsystem/component/button/ModalButton.kt
@@ -0,0 +1,31 @@
+package com.example.designsystem.component.button
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.designsystem.util.noRippleClickable
+
+@Composable
+fun ModalButton(
+    backgroundColor: Color,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    content: @Composable () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .background(
+                color = backgroundColor,
+            )
+            .noRippleClickable(onClick = onClick)
+            .padding(vertical = 16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        content()
+    }
+}
