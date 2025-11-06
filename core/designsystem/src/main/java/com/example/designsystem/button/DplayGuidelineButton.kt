e53ac67d1d6d19fcd07acda4758bcb844d2759fe
kamja0510 <kamja990510@gmail.com>
2025-11-06 20:46:28 +0900

    [feat] #25 DPlayGuidelineButton 구현

---
+++ core/designsystem/src/main/java/com/example/designsystem/button/DplayGuidelineButton.kt
@@ -0,0 +1,74 @@
+package com.example.designsystem.button
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dplay.designsystem.R
+import com.example.designsystem.theme.DPlayTheme
+
+@Composable
+fun DPlayGuidelineButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+){
+    DPlayButtonSlot(
+        modifier = modifier,
+        onClick = onClick,
+        paddingValues = PaddingValues(8.dp),
+        containerColor = DPlayTheme.colors.dplayWhite,
+        borderColor = DPlayTheme.colors.gray200,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_info_20),
+                contentDescription = stringResource(R.string.guideline_button_icon_description),
+                tint = Color.Unspecified
+            )
+
+            Spacer(
+                modifier = Modifier.size(8.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.guideline_button_label),
+                style = DPlayTheme.typography.capMed12,
+                color = DPlayTheme.colors.gray400
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DPlayGuidelineButtonPreview(){
+    DPlayTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(DPlayTheme.colors.dplayWhite)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            DPlayGuidelineButton()
+        }
+    }
+}
