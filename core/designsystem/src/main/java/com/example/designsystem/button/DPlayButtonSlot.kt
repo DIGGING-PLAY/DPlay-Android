79f2008e2c51b26893d080912a1a626a4d049d87
kamja0510 <kamja990510@gmail.com>
2025-11-06 20:04:59 +0900

    [feat] #25 DPlayButtonSlot 구현

---
+++ core/designsystem/src/main/java/com/example/designsystem/button/DPlayButtonSlot.kt
@@ -0,0 +1,57 @@
+package com.example.designsystem.button
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+import com.example.designsystem.util.noRippleClickable
+
+@Composable
+fun DPlayButtonSlot(
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    onClick: () -> Unit = {},
+    paddingValues: PaddingValues,
+    containerColor: Color,
+    borderColor: Color,
+    content: @Composable () -> Unit,
+){
+    Box(
+        modifier =
+            modifier
+                .clip(RoundedCornerShape(16.dp))
+                .border(
+                    width = 1.dp,
+                    color = borderColor,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .background(
+                    color = containerColor
+                )
+                .noRippleClickable(
+                    enabled = enabled,
+                    onClick = onClick,
+                    role = Role.Button
+                ).padding(paddingValues)
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            content()
+        }
+    }
+}
