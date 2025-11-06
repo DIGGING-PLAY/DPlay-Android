65a064529644736f5667d1fde95d7def8b542a5d
kamja0510 <kamja990510@gmail.com>
2025-11-06 21:29:43 +0900

    [feat] #25 DPlayKakaoLoginButton 구현

---
+++ core/designsystem/src/main/java/com/example/designsystem/button/DPlayKakaoLoginButton.kt
@@ -0,0 +1,78 @@
+package com.example.designsystem.button
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dplay.designsystem.R
+import com.example.designsystem.theme.DPlayTheme
+
+@Composable
+fun DPlayKakaoLoginButton(
+    modifier : Modifier = Modifier,
+    onClick: () -> Unit = {}
+){
+    DPlayButtonSlot(
+        modifier = modifier,
+        onClick = onClick,
+        paddingValues = PaddingValues(20.dp),
+        containerColor = DPlayTheme.colors.kakaoYellow,
+        borderColor = DPlayTheme.colors.kakaoYellow,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_kakao_24),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+
+            Text(
+                text = stringResource(R.string.kakao_login_button_label),
+                style = DPlayTheme.typography.bodyBold16,
+                color = DPlayTheme.colors.dplayBlack
+            )
+
+            Spacer(modifier = Modifier.size(24.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DPlayKakaoLoginButtonPreview(){
+    DPlayTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(DPlayTheme.colors.dplayWhite)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            DPlayKakaoLoginButton(
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
