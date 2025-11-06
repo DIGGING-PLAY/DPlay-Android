15330c4b5563096470225e754e82c44c0261f6cf
kamja0510 <kamja990510@gmail.com>
2025-11-07 02:24:12 +0900

    [feat] #29 DPlaySnackbar ui 구현

---
+++ core/designsystem/src/main/java/com/example/designsystem/snackbar/DPlaySnackbar.kt
@@ -0,0 +1,90 @@
+package com.example.designsystem.snackbar
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dplay.designsystem.R
+import com.example.designsystem.theme.DPlayTheme
+
+@Composable
+fun DPlaySnackbar(
+    @DrawableRes iconRes: Int,
+    description: String,
+    modifier: Modifier = Modifier,
+    content: (@Composable () -> Unit)? = null
+){
+    Box(
+        modifier = modifier
+            .clip(
+                shape = RoundedCornerShape(16.dp)
+            )
+            .background(
+                color = DPlayTheme.colors.gray500,
+            )
+            .padding(horizontal = 12.dp, vertical = 16.dp)
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Icon(
+                imageVector = ImageVector.vectorResource(iconRes),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Text(
+                text = description,
+                color = DPlayTheme.colors.dplayWhite,
+                style = DPlayTheme.typography.bodyMed14
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            content?.invoke()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DPlaySnackbarPreview(){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        DPlaySnackbar(
+            iconRes = R.drawable.ic_check_circle_pink_24,
+            description = "보관함에 추가했어요"
+        ){
+            Text(
+                text = "보러가기",
+                color = DPlayTheme.colors.dplayPink,
+                style = DPlayTheme.typography.bodyBold14
+            )
+        }
+    }
+}
