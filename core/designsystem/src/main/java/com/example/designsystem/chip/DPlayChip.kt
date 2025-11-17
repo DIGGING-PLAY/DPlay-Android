d8e2ce59f42560c757dd3f6579e43cb7646f7221
kamja0510 <kamja990510@gmail.com>
2025-11-17 23:38:36 +0900

    [chore] #31 ktlint

--- core/designsystem/src/main/java/com/example/designsystem/chip/DPlayChip.kt
+++ core/designsystem/src/main/java/com/example/designsystem/chip/DPlayChip.kt
@@ -3,7 +3,6 @@ package com.example.designsystem.chip
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,16 +10,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.designsystem.chip.type.DPlayChipType
@@ -31,21 +26,20 @@ import com.example.designsystem.theme.DPlayTheme
 fun DPlayChip(
     type: DPlayChipType,
     modifier: Modifier = Modifier,
-){
+) {
     Row(
-        modifier = modifier
-            .clip(
-                shape = RoundedCornerShape(16.dp)
-            )
-            .border(
-                width = 1.dp,
-                color = DPlayTheme.colors.dplayPink,
-                shape = RoundedCornerShape(16.dp)
-            )
-            .background(DPlayTheme.colors.dplayWhite)
-            .padding(horizontal = 12.dp,vertical = 6.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ){
+        modifier =
+            modifier
+                .clip(
+                    shape = RoundedCornerShape(16.dp),
+                ).border(
+                    width = 1.dp,
+                    color = DPlayTheme.colors.dplayPink,
+                    shape = RoundedCornerShape(16.dp),
+                ).background(DPlayTheme.colors.dplayWhite)
+                .padding(horizontal = 12.dp, vertical = 6.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
         DplayBaseIcon(
             iconRes = type.iconRes,
         )
@@ -55,22 +49,23 @@ fun DPlayChip(
         Text(
             text = stringResource(type.stringRes),
             style = DPlayTheme.typography.bodySemi14,
-            color = DPlayTheme.colors.dplayPink
+            color = DPlayTheme.colors.dplayPink,
         )
     }
 }
 
 @Preview
 @Composable
-fun DPlayChipPreview(){
+fun DPlayChipPreview() {
     DPlayTheme {
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(DPlayTheme.colors.dplayWhite)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ){
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .background(DPlayTheme.colors.dplayWhite)
+                    .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
             DPlayChip(type = DPlayChipType.NEW)
             DPlayChip(type = DPlayChipType.EDITOR)
             DPlayChip(type = DPlayChipType.BEST)
