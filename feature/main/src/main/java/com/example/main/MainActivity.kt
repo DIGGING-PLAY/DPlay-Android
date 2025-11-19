6c6057d979a516b414b5aa9560f4dbdc75190998
kamja0510 <kamja990510@gmail.com>
2025-11-19 23:15:09 +0900

    [feat] #42 MainScreen 구현

--- feature/main/src/main/java/com/example/main/MainActivity.kt
+++ feature/main/src/main/java/com/example/main/MainActivity.kt
@@ -20,31 +20,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navigator: MainNavigator = rememberMainNavigator()
             DPlayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
-                }
+                MainScreen(
+                    navigator = navigator,
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Greeting("Android")
-}
