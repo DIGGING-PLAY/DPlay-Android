88130214d1c64ac191d63a58fcc45912be986dbe
kamja0510 <kamja990510@gmail.com>
2025-11-26 04:39:01 +0900

    [feat] #42 navigation 모듈에 모든 Route 정의

--- core/navigation/src/main/java/com/example/navigation/Route.kt
+++ core/navigation/src/main/java/com/example/navigation/Route.kt
@@ -1,14 +1,28 @@
 package com.example.navigation
 
-import kotlinx.serialization.Serializable
+import androidx.annotation.DrawableRes
+import androidx.navigation3.runtime.NavKey
+import com.dplay.designsystem.R
 
-sealed interface Route {
-    @Serializable
-    data object Home : Route
-
-    @Serializable
-    data object Recommend : Route
-
-    @Serializable
-    data object MyPage : Route
+sealed interface TopLevelRoute {
+    @get:DrawableRes
+    val selectedIconRes: Int
+    @get:DrawableRes
+    val unselectedIconRes: Int
 }
+
+data object Home : TopLevelRoute, NavKey {
+    override val selectedIconRes: Int
+        get() = R.drawable.ic_home_active_32
+    override val unselectedIconRes: Int
+        get() = R.drawable.ic_home_disabled_32
+}
+
+data object MyPage : TopLevelRoute, NavKey {
+    override val selectedIconRes: Int
+        get() = R.drawable.ic_bookmark_active_32
+    override val unselectedIconRes: Int
+        get() = R.drawable.ic_bookmark_disabled_32
+}
+
+data object Recommend : NavKey
