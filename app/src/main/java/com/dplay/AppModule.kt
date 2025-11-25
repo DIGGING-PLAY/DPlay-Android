aeb9b2b4af073b3ff3e7f6402ff0506bf1a35145
kamja0510 <kamja990510@gmail.com>
2025-11-26 04:39:52 +0900

    [feat] #42 Navigator 주입 설정

---
+++ app/src/main/java/com/dplay/AppModule.kt
@@ -0,0 +1,18 @@
+package com.dplay
+
+import com.example.navigation.Home
+import com.example.navigation.Navigator
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+
+@Module
+@InstallIn(ActivityRetainedComponent::class)
+object AppModule {
+
+    @Provides
+    @ActivityRetainedScoped
+    fun provideNavigator() : Navigator = Navigator(startDestination = Home)
+}
