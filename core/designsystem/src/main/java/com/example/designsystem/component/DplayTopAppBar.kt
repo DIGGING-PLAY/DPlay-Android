39188be8b1fdeb120ac77a8a6f8b589c3041fbfd
kamja0510 <kamja990510@gmail.com>
2025-12-04 04:28:09 +0900

    [feat] #44 DplayTopAppBar의 containerColor 지정할 수 있도록 설정

--- core/designsystem/src/main/java/com/example/designsystem/component/DplayTopAppBar.kt
+++ core/designsystem/src/main/java/com/example/designsystem/component/DplayTopAppBar.kt
@@ -9,9 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dplay.designsystem.R
@@ -21,6 +24,7 @@ import com.example.designsystem.util.noRippleClickable
 @Composable
 fun DplayTopAppBar(
     modifier: Modifier = Modifier,
+    containerColor: Color = DPlayTheme.colors.dplayWhite,
     @DrawableRes leftIconRes: Int? = null,
     @DrawableRes rightIconRes: Int? = null,
     title: String? = null,
@@ -66,6 +70,9 @@ fun DplayTopAppBar(
                 }
             }
         },
+        colors = TopAppBarDefaults.topAppBarColors().copy(
+            containerColor = containerColor,
+        )
     )
 }
 
