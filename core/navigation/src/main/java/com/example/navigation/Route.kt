b2483f6dd7b5173b08151ea74fba8220d354774d
kamja0510 <kamja990510@gmail.com>
2025-12-04 02:14:23 +0900

    [feat] #44 온보딩 화면들 NavKey 정의

--- core/navigation/src/main/java/com/example/navigation/Route.kt
+++ core/navigation/src/main/java/com/example/navigation/Route.kt
@@ -27,7 +27,9 @@ data object MyPage : TopLevelRoute, NavKey {
 }
 
 data object Splash : NavKey
-
 data object Login : NavKey
-
+data object OnboardingTerms : NavKey
+data object OnboardingProfile : NavKey
+data object Onboarding : NavKey
+data object OnboardingPermission : NavKey
 data object Recommend : NavKey
