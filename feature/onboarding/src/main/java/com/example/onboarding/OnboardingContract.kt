c9cc55c100fb4c5fdee945f0c0ab0ecc2fb53a62
kamja0510 <kamja990510@gmail.com>
2025-12-05 08:19:23 +0900

    [feat] #44 OnboardingProfileScreen.kt에서 사용될 state, intent 정의

--- feature/onboarding/src/main/java/com/example/onboarding/OnboardingContract.kt
+++ feature/onboarding/src/main/java/com/example/onboarding/OnboardingContract.kt
@@ -1,25 +1,40 @@
 package com.example.onboarding
 
+import com.example.designsystem.component.textfield.type.InputState
 import com.example.ui.base.BaseContract
 
 class OnboardingContract {
     data class OnboardingState(
         val agreedTerms: Set<TermType> = emptySet(),
+
+        val nickname: String = "",
+        val profileImageUri: String? = null,
+        val nicknameInputState: InputState = InputState.Default,
+        val isAlbumLauncherBottomSheetVisible: Boolean = false,
     ) : BaseContract.State {
 
-        val isNextButtonEnabled: Boolean
+        val isTermsScreenNextButtonEnabled: Boolean
             get() = agreedTerms.containsAll(TermType.mandatoryTerms)
 
         val isAllTermsAgreed: Boolean
             get() = agreedTerms.size == TermType.entries.size
+
+        val isProfileScreenNextButtonEnabled: Boolean
+            get() = nicknameInputState is InputState.Success
     }
 
     sealed interface OnboardingIntent : BaseContract.Intent {
         data class OnToggleTerm(val term: TermType) : OnboardingIntent
-
         data object OnToggleAllTerms : OnboardingIntent
+        data object OnTermsScreenNextButtonClick : OnboardingIntent
 
-        data object OnNextButtonClick : OnboardingIntent
+        data class OnNicknameChanged(val input: String) : OnboardingIntent
+        data object OnProfileImageClick : OnboardingIntent
+        data object OnAlbumLauncherBottomSheetDismiss : OnboardingIntent
+        data object OnDefaultImageSelect : OnboardingIntent
+        data object OnAlbumLauncherSelect : OnboardingIntent
+        data class OnAlbumImageSelect(val uri: String?) : OnboardingIntent
+        data object OnProfileScreenNextButtonClick : OnboardingIntent
     }
 
     sealed interface OnboardingSideEffect : BaseContract.SideEffect
