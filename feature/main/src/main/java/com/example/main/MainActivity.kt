e244951c9cc26e8ca536170f3490046a54d089cd
t1nm1ksun <minsuk07672@naver.com>
2025-12-06 23:12:02 +0900

    delete test code

--- feature/main/src/main/java/com/example/main/MainActivity.kt
+++ feature/main/src/main/java/com/example/main/MainActivity.kt
@@ -27,19 +27,6 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding),
                     )
-                    DPlayLargeCover(
-                        isBookmarkChecked = true,
-                        isLikeChecked = false,
-                        likeCount = 24,
-                        writerProfileImageUrl = "",
-                        writerNickname = "윤서얌",
-                        content = "진짜 나오자마자 들었는데 이 노래가 최고! 출근곡, 퇴근곡, 노동곡 다 되는 짱제로! 일하는 매장에서도 수십 번씩 틀고 있어요. 모두가 알아야 돼..",
-                        musicImageUrl = "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/53/d4/c1/53d4c1e4-c712-ef2b-e862-f46045fe5500/cover_KM0022858_1.jpg/512x512bb.jpg",
-                        onStreamClick = {},
-                        onLikeClick = {},
-                        onBookmarkClick = {},
-                        isStreaming = true,
-                    )
                 }
             }
         }
