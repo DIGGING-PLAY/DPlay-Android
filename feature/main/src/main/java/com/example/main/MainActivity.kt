c7eef2939a1207856c0f6811bb9cfa5aacad5234
t1nm1ksun <minsuk07672@naver.com>
2025-12-06 23:48:53 +0900

    code formatting

--- feature/main/src/main/java/com/example/main/MainActivity.kt
+++ feature/main/src/main/java/com/example/main/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.designsystem.component.DPlayLargeCover
 import com.example.designsystem.theme.DPlayTheme
 import dagger.hilt.android.AndroidEntryPoint
 
