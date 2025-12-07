458081e1864a4f2e9cacdf76404aaab2ed0b27e0
kamja0510 <kamja990510@gmail.com>
2025-12-07 15:20:37 +0900

    [chore] #44 로그인 모듈 스트링 추출

--- feature/login/src/main/java/com/example/login/LoginScreen.kt
+++ feature/login/src/main/java/com/example/login/LoginScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -63,7 +64,7 @@ fun LoginScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "새로움을 발견하는 순간",
+            text = stringResource(com.dplay.login.R.string.login_title),
             style = DPlayTheme.typography.bodyBold16,
         )
 
