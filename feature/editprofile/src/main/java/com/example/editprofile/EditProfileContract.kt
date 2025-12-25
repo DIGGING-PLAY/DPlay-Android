ad6b1e621fb710ab4076ec6ed1c0dc494cf1175b
kamja0510 <kamja990510@gmail.com>
2025-12-26 06:38:57 +0900

    [feat] #55 EditProfileContract.kt 설계

--- feature/editprofile/src/main/java/com/example/editprofile/EditProfileContract.kt
+++ feature/editprofile/src/main/java/com/example/editprofile/EditProfileContract.kt
@@ -1,4 +1,47 @@
 package com.example.editprofile
 
+import android.net.Uri
+import com.example.designsystem.component.textfield.type.InputState
+import com.example.ui.base.BaseContract
+
 class EditProfileContract {
+    data class EditProfileState(
+        val nickname: String = "",
+        val nicknameInputState: InputState = InputState.Default,
+        val profileImageUri: Uri? = null,
+        val isAlbumLauncherBottomSheetVisible: Boolean = false,
+    ): BaseContract.State{
+        val isEditButtonEnabled: Boolean
+            get() = nicknameInputState is InputState.Success
+    }
+
+    sealed interface EditProfileIntent : BaseContract.Intent {
+        data object OnBackButtonClick : EditProfileIntent
+
+        data class OnNicknameChanged(
+            val input: String,
+        ) : EditProfileIntent
+
+        data object OnProfileImageClick : EditProfileIntent
+
+        data object OnAlbumLauncherBottomSheetDismiss : EditProfileIntent
+
+        data object OnDefaultImageSelect : EditProfileIntent
+
+        data object OnAlbumLauncherSelect : EditProfileIntent
+
+        data class OnAlbumImageSelect(
+            val uri: Uri?,
+        ) : EditProfileIntent
+
+        data object OnProfileScreenNextButtonClick : EditProfileIntent
+
+        data object OnEditButtonClick : EditProfileIntent
+    }
+
+    sealed interface EditProfileSideEffect : BaseContract.SideEffect {
+        data object LaunchAlbum : EditProfileSideEffect
+
+        data object NavigateToMyPage : EditProfileSideEffect
+    }
 }
