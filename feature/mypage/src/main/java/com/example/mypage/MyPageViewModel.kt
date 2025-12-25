2e2fe80cbb90db62ff4a25b166d072d3ca61f035
kamja0510 <kamja990510@gmail.com>
2025-12-26 06:00:59 +0900

    [feat] #55 MyPageViewModel 틀 잡아놓기

---
+++ feature/mypage/src/main/java/com/example/mypage/MyPageViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.mypage
+
+import com.example.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyPageViewModel
+    @Inject
+    constructor() : BaseViewModel<MyPageContract.MyPageState, MyPageContract.MyPageIntent, MyPageContract.MyPageSideEffect>(
+        MyPageContract.MyPageState(),
+    ) {
+    override fun handleIntent(intent: MyPageContract.MyPageIntent) {
+        when (intent) {
+            MyPageContract.MyPageIntent.OnBottomSheetCancelClick -> {
+
+            }
+
+            MyPageContract.MyPageIntent.OnBottomSheetDeleteClick -> {
+
+            }
+
+            MyPageContract.MyPageIntent.OnDialogueCancelClick -> {
+
+            }
+
+            MyPageContract.MyPageIntent.OnDialogueDeleteClick -> {
+                // 삭제 api
+            }
+
+            is MyPageContract.MyPageIntent.OnKebabIconClick -> {
+                setSideEffect(MyPageContract.MyPageSideEffect.ShowDeleteBottomSheet)
+            }
+
+            is MyPageContract.MyPageIntent.OnMusicItemClick -> {
+                setSideEffect(MyPageContract.MyPageSideEffect.NavigateToDetail(intent.musicId))
+            }
+
+            MyPageContract.MyPageIntent.OnProfileClick -> {
+                setSideEffect(MyPageContract.MyPageSideEffect.NavigateToEditProfile)
+            }
+
+            MyPageContract.MyPageIntent.OnSettingIconClick -> {
+                setSideEffect(MyPageContract.MyPageSideEffect.NavigateToSettings)
+            }
+
+            is MyPageContract.MyPageIntent.OnTabClick -> {
+                updateState {
+                    copy(selectedTabIndex = intent.tabIndex)
+                }
+            }
+        }
+    }
+}
