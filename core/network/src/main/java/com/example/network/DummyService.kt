8665f2520acb8bfafef7590f854b7fa66f8b82f2
kamja0510 <kamja990510@gmail.com>
2026-01-02 08:29:01 +0900

    [feat] #60 dummyService 구현

---
+++ core/network/src/main/java/com/example/network/DummyService.kt
@@ -0,0 +1,16 @@
+package com.example.network
+
+import com.example.network.NetworkConstants.API
+import com.example.network.NetworkConstants.VERSIONS
+import com.example.network.response.DummyResponse
+import kotlinx.serialization.InternalSerializationApi
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+@OptIn(InternalSerializationApi::class)
+interface DummyService {
+    @GET("$API/$VERSIONS/dummy/{dummyId}")
+    suspend fun getDummy(
+        @Path("dummyId") dummyId: Long,
+    ): BaseResponse<DummyResponse>
+}
