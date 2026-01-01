a95027cfa7ca4106abcc58011a6a4ea697db3376
kamja0510 <kamja990510@gmail.com>
2026-01-02 08:29:10 +0900

    [feat] #60 service 모듈 구현

---
+++ core/network/src/main/java/com/example/network/ServiceModule.kt
@@ -0,0 +1,18 @@
+package com.example.network
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+    @Provides
+    @Singleton
+    fun provideDummyService(
+        retrofit: Retrofit,
+    ): DummyService = retrofit.create(DummyService::class.java)
+}
