e71d76e676d748081f60f687ec43eba85b41a0c4
kamja0510 <kamja990510@gmail.com>
2026-01-02 08:25:29 +0900

    [feat] #60 network 모듈에서 쓰이는 상수 정리

---
+++ core/network/src/main/java/com/example/network/NetworkConstants.kt
@@ -0,0 +1,13 @@
+package com.example.network
+
+object NetworkConstants {
+    const val API = "api"
+
+    const val VERSIONS = "v1"
+
+    const val AUTH = "auth"
+    const val USERS = "users"
+    const val QUESTIONS = "questions"
+    const val POSTS = "posts"
+    const val TRACKS = "tracks"
+}
