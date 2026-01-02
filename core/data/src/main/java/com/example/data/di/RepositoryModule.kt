4cf8fef3f21b28b880fe903fccad4982ab3ca0c9
kamja0510 <kamja990510@gmail.com>
2026-01-02 09:36:12 +0900

    [feat] #60 repository module 구현

---
+++ core/data/src/main/java/com/example/data/di/RepositoryModule.kt
@@ -0,0 +1,18 @@
+package com.example.data.di
+
+import com.example.data.repository.DummyRepositoryImpl
+import com.example.domain.repository.DummyRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindsDummyRepository(repositoryImpl: DummyRepositoryImpl): DummyRepository
+}
