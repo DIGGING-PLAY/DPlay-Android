bff39496dfb7648b4ce923b0afb22a3f015f036c
kamja0510 <kamja990510@gmail.com>
2026-01-02 09:41:13 +0900

    [feat] #60 DummyViewModel.kt 에 적용

--- feature/dummy/src/main/java/com/example/dummy/DummyViewModel.kt
+++ feature/dummy/src/main/java/com/example/dummy/DummyViewModel.kt
@@ -1,18 +1,27 @@
 package com.example.dummy
 
+import androidx.lifecycle.viewModelScope
+import com.example.domain.repository.DummyRepository
 import com.example.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DummyViewModel
     @Inject
-    constructor() : BaseViewModel<DummyContract.DummyState, DummyContract.DummyIntent, DummyContract.DummySideEffect>(
+    constructor(
+        private val dummyRepository: DummyRepository,
+    ) : BaseViewModel<DummyContract.DummyState, DummyContract.DummyIntent, DummyContract.DummySideEffect>(
             DummyContract.DummyState(),
         ) {
         override fun handleIntent(intent: DummyContract.DummyIntent) {
             when (intent) {
-                DummyContract.DummyIntent.Initialize -> {}
+                DummyContract.DummyIntent.Initialize -> {
+                    viewModelScope.launch {
+                        dummyRepository.getDummy(dummyId = 1L).onSuccess { dummy -> }.onFailure {}
+                    }
+                }
                 is DummyContract.DummyIntent.OnClickNumberButton -> increment(intent.number)
             }
         }
