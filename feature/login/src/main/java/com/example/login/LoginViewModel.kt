4423923d10ace201990159b1265d4abb2277c39b
kamja0510 <kamja990510@gmail.com>
2026-01-07 08:37:48 +0900

    [feat] #63 LoginViewModel에 kakao login 붙이기

--- feature/login/src/main/java/com/example/login/LoginViewModel.kt
+++ feature/login/src/main/java/com/example/login/LoginViewModel.kt
@@ -1,20 +1,40 @@
 package com.example.login
 
+import androidx.lifecycle.viewModelScope
+import com.example.domain.repository.AuthRepository
 import com.example.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel
     @Inject
-    constructor() : BaseViewModel<LoginContract.LoginState, LoginContract.LoginIntent, LoginContract.LoginSideEffect>(
+    constructor(
+        private val authRepository: AuthRepository,
+    ) : BaseViewModel<LoginContract.LoginState, LoginContract.LoginIntent, LoginContract.LoginSideEffect>(
             LoginContract.LoginState(),
         ) {
         override fun handleIntent(intent: LoginContract.LoginIntent) {
             when (intent) {
                 LoginContract.LoginIntent.OnKakaoLogin -> {
-                    setSideEffect(LoginContract.LoginSideEffect.NavigateToOnboarding)
+                    kakaoLogin()
                 }
             }
         }
+
+    private fun kakaoLogin() {
+        viewModelScope.launch {
+            authRepository
+                .kakaoLogin()
+                .onSuccess { data ->
+                    when (data) {
+                        "로그인 성공" -> setSideEffect(LoginContract.LoginSideEffect.NavigateToHome)
+                        "회원가입 필요" -> setSideEffect(LoginContract.LoginSideEffect.NavigateToOnboarding)
+                    }
+                }.onFailure {
+                    // 카카오 로그인 실패 처리
+                }
+        }
     }
+}
