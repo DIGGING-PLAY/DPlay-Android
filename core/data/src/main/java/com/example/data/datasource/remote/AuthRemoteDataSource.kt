32534d276b7fe9539247fe905cd2e2d11e6e0a65
kamja0510 <kamja990510@gmail.com>
2026-01-07 09:08:35 +0900

    [feat] #63 AuthRemoteDataSource의 signup 함수 구현

--- core/data/src/main/java/com/example/data/datasource/remote/AuthRemoteDataSource.kt
+++ core/data/src/main/java/com/example/data/datasource/remote/AuthRemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.example.data.datasource.remote
 
 import com.example.data.model.request.LoginRequest
+import com.example.data.model.request.SignupRequest
 import com.example.data.model.response.BaseResponse
 import com.example.data.model.response.TokenResponse
 import com.example.data.service.AuthService
@@ -8,7 +9,13 @@ import com.example.network.NetworkException
 import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.HttpException
+import java.io.File
 import javax.inject.Inject
 
 @OptIn(InternalSerializationApi::class)
@@ -16,6 +23,7 @@ class AuthRemoteDataSource
 @Inject
 constructor(
     private val authService: AuthService,
+    private val json: Json,
 ) {
     suspend fun login(
         accessToken: String,
@@ -35,7 +43,7 @@ constructor(
 
                 if (errorString != null) {
                     try {
-                        val errorResponse = Json.decodeFromString<BaseResponse<String?>>(errorString)
+                        val errorResponse = json.decodeFromString<BaseResponse<String?>>(errorString)
 
                         if (errorResponse.code == 4041) {
                             throw NetworkException(4041, errorResponse.message)
@@ -50,5 +58,28 @@ constructor(
 
             throw e
         }
+    }
+
+    suspend fun signup(
+        imageFile: File?,
+        signupRequest: SignupRequest
+    ): TokenResponse {
+
+        val imagePart = if (imageFile != null) {
+            val requestFile = imageFile.asRequestBody("image/*".toMediaTypeOrNull())
+            MultipartBody.Part.createFormData("profileImg", imageFile.name, requestFile)
+        } else {
+            null
+        }
+
+        val jsonString = json.encodeToString(signupRequest)
+        val requestPart = jsonString.toRequestBody("application/json".toMediaType())
+
+        val response = authService.signup(
+            profileImg = imagePart,
+            request = requestPart
+        )
+
+        return response.data!!
     }
 }
