05c25ec49aa432373f7e92fc8cd7e758287f0e08
kamja0510 <kamja990510@gmail.com>
2026-01-10 02:02:41 +0900

    [feat] #63 UserRepository 인터페이스 구현 및 구현체 구현

---
+++ core/data/src/main/java/com/example/data/repository/UserRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package com.example.data.repository
+
+import com.example.data.datasource.local.UserLocalDataSource
+import com.example.domain.model.User
+import com.example.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val userLocalDataSource: UserLocalDataSource
+) : UserRepository {
+    override fun getUser(): Flow<User?> {
+        return userLocalDataSource.userFlow
+    }
+}
