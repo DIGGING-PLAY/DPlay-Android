e5f81ba5626ef02892df7d01affaa4b7dd76b1cc
kamja0510 <kamja990510@gmail.com>
2026-01-13 13:53:03 +0900

    [feat] #63 UserRemoteDataSource 생성하고 patchProfile 함수 구현

---
+++ core/data/src/main/java/com/example/data/datasource/remote/UserRemoteDataSource.kt
@@ -0,0 +1,38 @@
+package com.example.data.datasource.remote
+
+import com.example.data.service.UserService
+import kotlinx.serialization.InternalSerializationApi
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import timber.log.Timber
+import java.io.File
+import javax.inject.Inject
+
+@OptIn(InternalSerializationApi::class)
+class UserRemoteDataSource @Inject constructor(
+    private val userService : UserService
+) {
+    suspend fun patchProfile(
+        nickname: String?,
+        imageFile: File?,
+    ){
+        try {
+            val imagePart =
+                if (imageFile != null) {
+                    val requestFile = imageFile.asRequestBody("image/*".toMediaTypeOrNull())
+                    MultipartBody.Part.createFormData("profileImg", imageFile.name, requestFile)
+                } else {
+                    null
+                }
+
+            userService.patchProfile(
+                profileImg = imagePart,
+                request = nickname,
+            )
+        } catch (e: Exception) {
+            Timber.e(e)
+            throw e
+        }
+    }
+}
--- core/data/src/main/java/com/example/data/repository/UserRepositoryImpl.kt
+++ core/data/src/main/java/com/example/data/repository/UserRepositoryImpl.kt
@@ -1,10 +1,14 @@
 package com.example.data.repository
 
+import com.example.data.datasource.local.FileLocalDataSource
 import com.example.data.datasource.local.TokenLocalDataSource
 import com.example.data.datasource.local.UserLocalDataSource
+import com.example.data.datasource.remote.UserRemoteDataSource
+import com.example.domain.model.ProfileImageState
 import com.example.domain.model.User
 import com.example.domain.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
+import timber.log.Timber
 import javax.inject.Inject
 
 class UserRepositoryImpl
@@ -12,10 +16,42 @@ class UserRepositoryImpl
     constructor(
         private val userLocalDataSource: UserLocalDataSource,
         private val tokenLocalDataSource: TokenLocalDataSource,
+        private val userRemoteDataSource: UserRemoteDataSource,
+        private val fileLocalDataSource: FileLocalDataSource,
     ) : UserRepository {
         override fun getUser(): Flow<User?> = userLocalDataSource.userFlow
 
         override fun getAccessToken(): Flow<String?> = tokenLocalDataSource.accessToken
 
         override fun getRefreshToken(): Flow<String?> = tokenLocalDataSource.refreshToken
+
+        override suspend fun updateProfile(
+            nickname: String?,
+            profileImageState: ProfileImageState
+        ): Result<Unit> = runCatching {
+
+            val profileFile = when(profileImageState){
+                is ProfileImageState.Keep -> null
+                is ProfileImageState.Delete -> fileLocalDataSource.createEmptyFile()
+                is ProfileImageState.Update -> fileLocalDataSource.createAndGetFile(profileImageState.imagePath)
+            }
+
+            userRemoteDataSource.patchProfile(
+                imageFile = profileFile,
+                nickname = nickname,
+            )
+
+            userLocalDataSource.updateNickname(nickname.orEmpty())
+            when(profileImageState){
+                ProfileImageState.Delete -> {
+                    userLocalDataSource.removeProfileImage()
+                }
+                is ProfileImageState.Update -> {
+                    userLocalDataSource.updateProfileImage(
+                        profileImagePath = profileImageState.imagePath
+                    )
+                }
+                ProfileImageState.Keep -> {}
+            }
+        }
     }
