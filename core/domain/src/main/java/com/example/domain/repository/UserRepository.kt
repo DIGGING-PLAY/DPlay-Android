540e8a9de0e01c886005ebe295d28d62cb483f32
kamja0510 <kamja990510@gmail.com>
2026-01-13 13:51:05 +0900

    [feat] #63 프로필 업데이트 repository 함수 정의

--- core/domain/src/main/java/com/example/domain/repository/UserRepository.kt
+++ core/domain/src/main/java/com/example/domain/repository/UserRepository.kt
@@ -1,5 +1,6 @@
 package com.example.domain.repository
 
+import com.example.domain.model.ProfileImageState
 import com.example.domain.model.User
 import kotlinx.coroutines.flow.Flow
 
@@ -9,4 +10,9 @@ interface UserRepository {
     fun getAccessToken(): Flow<String?>
 
     fun getRefreshToken(): Flow<String?>
+
+    suspend fun updateProfile(
+        nickname: String?,
+        profileImageState: ProfileImageState,
+    ): Result<Unit>
 }
