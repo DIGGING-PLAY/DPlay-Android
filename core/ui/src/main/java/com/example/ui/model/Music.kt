8424ff8964bcfc7092f56fbdcc831867b8bfae82
kamja0510 <kamja990510@gmail.com>
2026-01-14 22:39:41 +0900

    [feat] #73 도메인 트랙 정보를 presentation 레이어의 music 객체로 변환하는 mapper 구현

--- core/ui/src/main/java/com/example/ui/model/Music.kt
+++ core/ui/src/main/java/com/example/ui/model/Music.kt
@@ -1,8 +1,17 @@
 package com.example.ui.model
 
+import com.example.domain.model.TrackInfo
+
 data class Music(
     val trackId: String,
     val musicTitle: String,
     val artistName: String,
     val thumbnailUrl: String?,
+)
+
+fun TrackInfo.toUiState(): Music = Music(
+    trackId = trackId,
+    musicTitle = songTitle,
+    artistName = artistName,
+    thumbnailUrl = coverImg
 )
