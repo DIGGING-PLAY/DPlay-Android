ed9888a71fa5d0fd689f04ed689330ca73fcd9b3
kamja0510 <kamja990510@gmail.com>
2026-01-14 22:39:00 +0900

    [feat] #73 TrackRepository 구현체 구현

---
+++ core/data/src/main/java/com/example/data/repository/TrackRepositoryImpl.kt
@@ -0,0 +1,32 @@
+package com.example.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.example.data.datasource.remote.TrackPagingSource
+import com.example.data.service.TrackService
+import com.example.domain.model.TrackInfo
+import com.example.domain.repository.TrackRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class TrackRepositoryImpl
+@Inject constructor(
+    private val trackService: TrackService
+): TrackRepository {
+    override fun searchTracks(query: String): Flow<PagingData<TrackInfo>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                enablePlaceholders = false,
+            ),
+            pagingSourceFactory = { TrackPagingSource(trackService, query) }
+        ).flow.map { pagingData ->
+            pagingData.map { track ->
+                track.toDomain()
+            }
+        }
+    }
+}
