7d51e1c4ac59d8a72d7234dfd000f26d668d766d
kamja0510 <kamja990510@gmail.com>
2026-01-15 18:22:28 +0900

    [feat] #73 ScrappedTracksPagingSource 구현

--- core/data/src/main/java/com/example/data/datasource/remote/ScrappedTracksPagingSource.kt
+++ core/data/src/main/java/com/example/data/datasource/remote/ScrappedTracksPagingSource.kt
@@ -2,34 +2,30 @@ package com.example.data.datasource.remote
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import com.example.data.model.response.RegisteredTrackResponse
+import com.example.data.model.response.ScrappedTrackResponse
 import com.example.data.service.UserService
 import kotlinx.serialization.InternalSerializationApi
 
 @OptIn(InternalSerializationApi::class)
-class RegisteredTracksPagingSource(
+class ScrappedTracksPagingSource(
     private val userService: UserService,
     private val userId: Long,
-    private val onTotalCountFetched: (Int) -> Unit
-): PagingSource<String, RegisteredTrackResponse>() {
-    override fun getRefreshKey(state: PagingState<String, RegisteredTrackResponse>): String? {
+): PagingSource<String, ScrappedTrackResponse>() {
+    override fun getRefreshKey(state: PagingState<String, ScrappedTrackResponse>): String? {
         return null
     }
 
-    override suspend fun load(params: LoadParams<String>): LoadResult<String, RegisteredTrackResponse> {
+    override suspend fun load(params: LoadParams<String>): LoadResult<String, ScrappedTrackResponse> {
         return try {
             val currentCursor = params.key
 
-            val response = userService.getRegisteredTracks(
+            val response = userService.getScrappedTracks(
                 userId = userId,
                 page = currentCursor,
                 size = null
             )
 
             val data = response.data ?: throw Exception("data is null")
-            if (params.key == null) {
-                onTotalCountFetched(data.totalCount)
-            }
             val tracks = data.items
             val nextCursor = data.nextCursor
 
