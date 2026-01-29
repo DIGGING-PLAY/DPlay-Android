8e34ffcde53ff82fcfcc043c71ee3fe58cddeaed
kamja0510 <kamja990510@gmail.com>
2026-01-30 05:06:04 +0900

    [feat] #93 릴리즈 설정
    keystore 읽고 signingConfigs 만들기
    난독화 적용
    리소스 축소 적용
    signingConfig 적용

--- build-logic/convention/src/main/java/com/example/convention/plugin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/example/convention/plugin/AndroidApplicationConventionPlugin.kt
@@ -10,6 +10,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import java.io.FileInputStream
 import java.util.Properties
 
 class AndroidApplicationConventionPlugin: Plugin<Project> {
@@ -21,6 +22,12 @@ class AndroidApplicationConventionPlugin: Plugin<Project> {
                     apply("org.jlleitschuh.gradle.ktlint")
                 }
 
+            val keystoreProperties = Properties()
+            val keystorePropertiesFile = rootProject.file("local.properties")
+            if (keystorePropertiesFile.exists()) {
+                keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+            }
+
             extensions.configure<ApplicationExtension> {
                 configureKotlinAndroid(this)
                 configureComposeAndroid(this)
@@ -31,8 +38,29 @@ class AndroidApplicationConventionPlugin: Plugin<Project> {
                     versionName = libs.getVersion("versionName").requiredVersion
                 }
 
-                dependencies{
-                    add("implementation",libs.getLibrary("timber"))
+                signingConfigs {
+                    create("release") {
+                        keyAlias = keystoreProperties["keyAlias"] as String?
+                        keyPassword = keystoreProperties["keyPassword"] as String?
+                        storeFile = (keystoreProperties["storeFile"] as String?)?.let { rootProject.file(it) }
+                        storePassword = keystoreProperties["storePassword"] as String?
+                    }
+                }
+
+                buildTypes {
+                    release {
+                        isMinifyEnabled = true
+                        isShrinkResources = true
+                        proguardFiles(
+                            getDefaultProguardFile("proguard-android-optimize.txt"),
+                            "proguard-rules.pro",
+                        )
+                        signingConfig = signingConfigs.getByName("release")
+                    }
+                }
+
+                dependencies {
+                    add("implementation", libs.getLibrary("timber"))
                 }
             }
         }
